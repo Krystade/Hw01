0059e1e1e9b5ad35f849c89522a5c6822cf426c9
Krys <21113012+Krystade@users.noreply.github.com>
2020-01-14 17:51:43 -0800

    Create hw01.h

---
+++ hw01.h
@@ -0,0 +1,17 @@
+/*
+ * hw01.h
+ *
+ *  Created on: Jan 14, 2020
+ *      Author: jschmid20
+ */
+
+#ifndef HW02_H_
+#define HW02_H_
+
+#include <iostream>
+using namespace std;
+
+void printArr(int arr[], int size);
+void selectionSort(int arr[], int size);
+
+#endif /* HW02_H_ */
